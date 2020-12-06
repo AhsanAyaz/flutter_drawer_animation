d53e995d3d907129fa6f768247bf28d763cf3f12
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-12-06 02:59:30 +0100

    chore(part2): added slide gesture handlers and fling velocity calculation for smoothness

--- lib/components/AppDrawer.dart
+++ lib/components/AppDrawer.dart
@@ -13,6 +13,10 @@ class AppDrawer extends StatefulWidget {
 class _AppDrawerState extends State<AppDrawer> with SingleTickerProviderStateMixin {
   static Duration duration = Duration(milliseconds: 300);
   AnimationController _controller;
+  static const double maxSlide = 255;
+  static const dragRightStartVal = 60;
+  static const dragLeftStartVal = maxSlide - 20;
+  static bool shouldDrag = false;
 
   @override
   void initState() {
@@ -38,10 +42,44 @@ class _AppDrawerState extends State<AppDrawer> with SingleTickerProviderStateMix
     super.dispose();
   }
 
+  void _onDragStart(DragStartDetails startDetails) {
+    bool isDraggingFromLeft = _controller.isDismissed && startDetails.globalPosition.dx < dragRightStartVal;
+    bool isDraggingFromRight = _controller.isCompleted && startDetails.globalPosition.dx > dragLeftStartVal;
+    shouldDrag = isDraggingFromLeft || isDraggingFromRight;
+  }
+
+  void _onDragUpdate(DragUpdateDetails updateDetails) {
+    if (shouldDrag == false) {
+      return;
+    }
+    double delta = updateDetails.primaryDelta / maxSlide;
+    _controller.value += delta;
+  }
+
+  void _onDragEnd(DragEndDetails dragEndDetails) {
+    if (_controller.isDismissed || _controller.isCompleted) {
+      return;
+    }
+
+    double _kMinFlingVelocity = 365.0;
+    double dragVelocity = dragEndDetails.velocity.pixelsPerSecond.dx.abs();
+
+    if (dragVelocity >= _kMinFlingVelocity) {
+      double visualVelocityInPx = dragEndDetails.velocity.pixelsPerSecond.dx / MediaQuery.of(context).size.width;
+      _controller.fling(velocity: visualVelocityInPx);
+    } else if (_controller.value < 0.5) {
+      close();
+    } else {
+      open();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    double maxSlide = 255;
     return GestureDetector(
+      onHorizontalDragStart: _onDragStart,
+      onHorizontalDragUpdate: _onDragUpdate,
+      onHorizontalDragEnd: _onDragEnd,
       child: AnimatedBuilder(
         animation: _controller,
         builder: (BuildContext context, _) {
